2dee5e56c083713a126642066573c23f1019aca2
sandikodev <androxoss@hotmail.com>
2026-02-25 04:53:50 +0700

    chore: add A11y diagnostic scripts and validation logs

---
+++ fix-a11y-4.cjs
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const path = require('path');
+const directoryPath = '/home/dev/project/roti-sholawat/src/components';
+
+function processDirectory(directory) {
+    fs.readdirSync(directory).forEach(file => {
+        const fullPath = path.join(directory, file);
+        if (fs.statSync(fullPath).isDirectory()) {
+            processDirectory(fullPath);
+        } else if (fullPath.endsWith('.svelte')) {
+            processFile(fullPath);
+        }
+    });
+}
+
+function processFile(file) {
+    let content = fs.readFileSync(file, 'utf8');
+    let originalContent = content;
+
+    content = content.replace(/role="button" tabindex="0" on:click={([^}]+)} on:keydown={([^}]+)}/g, (match, clickHandler, keydownHandler) => {
+        return `role="button" tabindex="0" onclick={${clickHandler}} onkeydown={${keydownHandler}}`;
+    });
+
+    if (content !== originalContent) {
+        console.log('Fixed on:click to onclick in:', file);
+        fs.writeFileSync(file, content);
+    }
+}
+
+processDirectory(directoryPath);
